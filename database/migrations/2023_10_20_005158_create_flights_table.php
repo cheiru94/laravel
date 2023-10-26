58d0faea069c7e8d83daf0145af8abef64c4689a
cheiru94 <cheiru94@gmail.com>
2023-10-27 01:41:20 +0900

    라라벨은 기본으로 notnull

--- database/migrations/2023_10_20_005158_create_flights_table.php
+++ database/migrations/2023_10_20_005158_create_flights_table.php
@@ -28,19 +28,25 @@ return new class extends Migration
      * Run the migrations.
      */
     //  ✏️ php artisan migrate 명령어가 실행되면 up 메소드가 실행된다 
-    public function up(): void
+    public function up(): void   
     {
-        Schema::create('flights', function (Blueprint $table) {
+        // 🍑 Schema::create(테이블이름 , 테이블 구조정의 하는 클로저 ) 🍑 
+        Schema::create('flights', function (Blueprint $table) {// 무명함수 : 클로저  -> 자동으로 생성되는 이 클로저 함수가 실행 되어서 flights 라는 테이블을 만들어 준다
              /* 🟢 추가할 컬럼들 작성 🟢 */
-            // UnsignedBigInt 타입의 auto_increament primary key id컬럼 생성.
-            $table->id();  // 프라이머리 키로 생성
+           
+             // 라라벨에서는 기본으로 notnull이다
+             //table은 위의 function 함수 안에 있다 (무명함수)
+            $table->id();  // 프라이머리 키로 생성 ->  UnsignedBigInt(무조건 양수로 저장된다) 타입의 auto_increament primary key id컬럼 생성.
             $table->string("name");  /* name이라는 칼럼을 문자열 저정할 수 있는 타입으로 만들어라 */
             $table->string("airline");  /* airline이라는 칼럼을 문자열 저정할 수 있는 타입으로 만들어라 */
 
             // datatime 데이타 타입으로 created_at, updated_at 이라는 두개의 칼럼을 만들어 준다.
-            $table->timestamps();// 만들어지는 테이블에 2개의 칼럼을 추가 시킨다.    1.created_at:레코드가 삽입되는 시간  2.updated_at:레코드가 변경되는 시간  => 자동으로 생성해 준다. 
+            $table->timestamps();// 만들어지는 테이블에 2개의 칼럼을 추가 시킨다.    1.created_at:레코드가 삽입되는 시간  2.updated_at:레코드가 변경되는 시간  => 자동으로 생성해 준다. 이렇게 2개를 만들어 주기 때문에 끝에 s가 붙는다 
         });
     }
+       // 테이터 타입을 칼럼마다 다르게 주는 이유는?
+       // => 타입에 맞는 연산을 하기 위해서이다. 숫자는 숫자 끼리, 날짜는 날짜끼리
+
 
     /**
      * Reverse the migrations.
