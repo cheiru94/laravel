759fe1dcc4558c77ae0c192c325cba075e62ff4e
cheiru94 <cheiru94@gmail.com>
2023-11-07 12:40:22 +0900

    roles 테이블

---
+++ database/migrations/2023_11_07_033047_create_roles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.   //   sail artisan make:model Role -m 이렇게 -m 옵션을 주니 마이그레이션 파일이 생성된다
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
