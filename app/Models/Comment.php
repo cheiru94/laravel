f7c2b903b1e9712bf0ec06ff815cf14a97bcffbc
cheiru94 <cheiru94@gmail.com>
2023-12-07 12:50:37 +0900

    $touches = ['post']

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,17 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory; 
+  use HasFactory;
 
-    protected $fillable = ['content', 'post_id','user_id'];
-    
-    public function post(){           // _id 이렇게 관례를 맞춰서 작성해주면  생략 가능
-        // return $this->belongsTo(Post::class,'post_id','id');
-        return $this->belongsTo(Post::class);
-    }
+  protected $fillable = ['content', 'post_id', 'user_id'];
 
-    public function user(){
-        return $this->belongsTo(User::class);
-    }
-    
+  protected $touches = ['post'];
+
+  public function post()
+  {           // _id 이렇게 관례를 맞춰서 작성해주면  생략 가능
+    // return $this->belongsTo(Post::class,'post_id','id');
+    return $this->belongsTo(Post::class);
+  }
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    // User과의 N:M 관계를 정의하는 메서드를 정의하자
-    // role_user 테이블에서 role_id가 roles 테이블의 id와 같은 레코드를 찾고 
-    // 그 레코드들의 user_id 값을 id값으로 가지는 레코드들을 users 테이블에서 찾는다. 
-    // public function users(){
-    //   return $this->belongsToMany(User::class); 
-    // } 
-    // 🟢 위에는 이게 생략된 내용이다.
-    public function users(){
-      return $this->belongsToMany(User::class,'role_user','role_id','user_id','id','id')->withTimestamps(); 
-      // 관계를 정의하기 위한 테이블에 타임스템프를 넣고 싶으면 이렇게 직접 지시를 해야 타임스탬프가 생긴다.
-    } 
+  // User과의 N:M 관계를 정의하는 메서드를 정의하자
+  // role_user 테이블에서 role_id가 roles 테이블의 id와 같은 레코드를 찾고 
+  // 그 레코드들의 user_id 값을 id값으로 가지는 레코드들을 users 테이블에서 찾는다. 
+  // public function users(){
+  //   return $this->belongsToMany(User::class); 
+  // } 
+
+  // 🟢 위에는 이게 생략된 내용이다.
+  public function users()
+  {
+    return $this->belongsToMany(User::class, 'role_user', 'role_id', 'user_id', 'id', 'id')->withTimestamps();
+    // 관계를 정의하기 위한 테이블에 타임스템프를 넣고 싶으면 이렇게 직접 지시를 해야 타임스탬프가 생긴다.
+  }
 }
