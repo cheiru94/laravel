75e56e03a75a8b91bd35c23c5023824972213e0d
cheiru94 <cheiru94@gmail.com>
2023-12-07 11:51:57 +0900

    .

--- database/migrations/2023_10_27_022429_create_comments_table.php
+++ database/migrations/2023_10_27_022429_create_comments_table.php
@@ -6,25 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->text('content');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('post_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('comments', function (Blueprint $table) {
+      $table->id();
+      $table->text('content');
+      $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+      $table->foreignId('post_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('comments');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('comments');
+  }
 };
